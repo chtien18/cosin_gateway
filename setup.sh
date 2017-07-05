635ac1132d4c40fad530bb826cae44ed8abbff46
Tien Cao-hoang <chtien18@gmail.com>
2017-07-05 13:42:50 +0700

    add sudo setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
-# setup script for Orange Pi
+# setup script for Raspberry Pi
 
-cd
-
-apt install python-pip -y
+sudo apt-get install python-pip -y
 
 # Dependency for network manager
-apt install isc-dhcp-server
+sudo apt-get install isc-dhcp-server
 
-pip install Flask Flask-bootstrap Flask-moment
+sudo pip install Flask Flask-bootstrap Flask-moment
 
 cd consin_gateway
 
